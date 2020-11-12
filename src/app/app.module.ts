c067c0e98c9ea6a7c8e40723e58a25de8f6b3a69
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-12 11:30:33 +0530

    services-http-catalog-parent-child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { AngmaterialModule } from './angmaterial/angmaterial.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,7 @@ import { MenuBarComponent } from './common/menu-bar/menu-bar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ProductComponent } from './common/product/product.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +19,15 @@ import { ContactComponent } from './pages/contact/contact.component';
     MenuBarComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngmaterialModule
+    AngmaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../services/product.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+productlist;
 
-  constructor() { }
+  constructor(private ps: ProductService) { }
 
   ngOnInit(): void {
+      this.ps.getProducts().subscribe(
+        (data: any) => this.productlist = data,
+        () => this.productlist = []
+      );
   }
 
 }
